03b462b6d8a3bc98a5fddd77e94b2250f6451dfe
johannranudd <33978097+johannranudd@users.noreply.github.com>
2022-10-20 21:18:36 +0200

    dad jokes

--- js/joker.js
+++ js/joker.js
@@ -1,7 +1,7 @@
-// import joker from 'one-liner-joke';
-
-// console.log(joker.getRandomJoke().body);
-
 import joker from 'give-me-a-joke';
 
-joker.getRandomDadJoke(console.log);
+export function dadJoke() {
+  return new Promise(function (resolve, reject) {
+    joker.getRandomDadJoke(resolve);
+  });
+}
